d8f4f22e9b9e9ac303779c5a477db1f5c342a37c
Brian Lalor <blalor@bravo5.org>
2011-02-08 11:11:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Entourage to iCal export script
+===============================
+
+I'm stuck using Microsoft Entourage 2004 at work. I wanted to have access to
+my calendar via iCal and on my iPhone (via MobileMe). This script exports the
+first Exchange calendar configured in Entourage to `~/Sites/entourage.ics`.
+You can then [subscribe to it via iCal][wc], provided you've got Web Sharing
+turned on.
+
+If you want access to the calendar via MobileMe, I'm pretty sure that
+`entourage.ics` will need to be accessible from the Internet. In that case,
+you'll probably want to scp it somewhere, or (maybe?) stick the file in your
+iDisk.
+
+INSTALLATION
+------------
+
+First, you'll need to [download my fork][fork] of the Python iCalendar module.  Unpack it and then run
+
+    python setup.py install
+
+Then, modify `org.bravo5.EntourageExport.plist` and replace the value of
+`ProgramArguments` with the path to `export_entourage_cal.py`. Finally, copy
+(or symlink) `org.bravo5.EntourageExport.plist` to `~/Library/LaunchAgents/`
+and run
+
+    launchctl load ~/Library/LaunchAgents/org.bravo5.EntourageExport.plist
+
+`launchd` will then export your calendar every 15 minutes.
+
+[wc]: webcal://localhost/~USERNAME/entourage.ics
+[fork]: https://github.com/blalor/iCalendar/archives/master
---
+++ export_entourage_cal.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import icalendar
+assert "2.2bjl" in icalendar.__file__, "wrong icalendar; use mine: https://github.com/blalor/iCalendar"
+
+import Cocoa
+import Foundation
+import PyObjCTools.AppHelper
+import ScriptingBridge
+
+import os
+
+# enum EntourageEFBt {
+#         EntourageEFBtBusy = 'eSBu' /* busy */,
+#         EntourageEFBtFree = 'eSFr' /* free */,
+#         EntourageEFBtTentative = 'eSTe' /* tentative */,
+#         EntourageEFBtOutOfOffice = 'eSOO' /* out of office */
+# };
+
+def enum_to_int(enum):
+    # http://stackoverflow.com/questions/548289/what-is-the-type-of-an-enum-whose-values-appear-to-be-strings/548305#548305
+    return ord(enum[0]) << 24 | ord(enum[1]) << 16 | ord(enum[2]) << 8 | ord(enum[3])
+
+
+FREE_BUSY_STATUS = {
+    enum_to_int('eSBu') : "busy",
+    enum_to_int('eSFr') : "free",
+    enum_to_int('eSTe') : "tentative",
+    enum_to_int('eSOO') : "out of office",
+}
+
+
+app = ScriptingBridge.SBApplication.applicationWithURL_(Foundation.NSURL.fileURLWithPath_("/Applications/Microsoft Office 2004/Microsoft Entourage"))
+exchangeAccount = app.ExchangeAccounts()[0]
+
+ical = None
+
+for cal in exchangeAccount.calendars():
+    for evt in cal.events():
+        # print evt.subject(), evt.startTime(), FREE_BUSY_STATUS[evt.freeBusyStatus()]
+        
+        ## correct illegal (according to iCal) property name
+        ## iCal[63106] <Warning>: Invalid char _ for PropertyName in line 1518
+        _ical_data = evt.iCalData()
+        _ical_data = _ical_data.replace("X-ENTOURAGE_UUID", "X-ENTOURAGE-UUID")
+        
+        c = icalendar.Calendar.from_string(_ical_data.encode('utf-8'))
+        
+        if ical == None:
+            ical = c
+        
+        for component in c.subcomponents:
+            if component not in ical.subcomponents:
+                if type(component) == icalendar.Event:
+                    
+                    ## find my attendee entry and set my status to ACCEPTED
+                    if 'attendee' in component:
+                        attendees = component['attendee']
+                        if type(attendees) != type([]):
+                            attendees = [attendees]
+                            
+                        for attendee in attendees:
+                            # print attendee
+                            if "brian.lalor@pearson.com" in attendee.lower():
+                                if FREE_BUSY_STATUS[evt.freeBusyStatus()] in ("free", "tentative"):
+                                    attendee.params['PARTSTAT'] = "TENTATIVE"
+                                else:
+                                    attendee.params['PARTSTAT'] = "ACCEPTED"
+                
+                ical.add_component(component)
+            
+        
+    
+ofp = open(os.path.expand("~/Sites/entourage.ics"), "wb")
+ofp.write(ical.as_string())
+ofp.close()
